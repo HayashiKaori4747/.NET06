9bdd6749678052760d0c653220919d1dc6ef9910
Kaori <hayashikaori4747@gmail.com>
2026-04-21 22:07:00 +0900

    first commit

---
+++ Pages/Transaction.cs
@@ -0,0 +1,10 @@
+using System;
+namespace DUAN1.Pages
+{
+    public class Transaction
+    {
+        public string Type { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
